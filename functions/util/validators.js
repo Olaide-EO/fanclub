2d448c85ada41d81c3f19f25bda502ba46c72e07
Olaide-EO <30773360+Olaide-EO@users.noreply.github.com>
2019-12-01 14:19:12 +0100

    correct typo in error validatesignupdata

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -22,7 +22,7 @@ const isEmail = (email) => {
 
     if(isEmpty(data.password)) errors.password = "Must not be empty";
     if(data.password !== data.confirmPassword) errors.confirmPassword = 'Passwords must match'
-    if(isEmpty(data.handle)) erros.handle = 'Must not be empty';
+    if(isEmpty(data.handle)) errors.handle = 'Must not be empty';
     
     return {
         errors,
